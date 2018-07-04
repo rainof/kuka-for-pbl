e497163ce58d1c90fa6096c60643433f47df759a
pattaraporn-t <tulathum.pattaraporn.ti8@is.naist.jp>
2018-07-04 20:38:18 +0900

    Can repeat moving

--- src/wiggle.cpp
+++ src/wiggle.cpp
@@ -9,46 +9,38 @@
 // This function sends positions to the kuka periodically for a wiggley motion.
 // Start the robot in fully extended position.
 
-ros::NodeHandle n;
-iiwa_msgs::JointPosition my_joint_position;
-ros::Publisher sendmotion_pub =  n.advertise<iiwa_msgs::JointPosition>("iiwa/command/JointPosition", 1000);
+// void callback1(const ros::TimerEvent&)
+// {
+//   ROS_INFO("Callback 1 triggered");
+//   // my_joint_position.position.a1 = 0;
+//   // my_joint_position.position.a2 = -1.5;
+//   // my_joint_position.position.a3 = 0;
+//   // my_joint_position.position.a4 = 0;
+//   // my_joint_position.position.a5 = 0;
+//   // my_joint_position.position.a6 = 0;
+//   // my_joint_position.position.a7 = 0;
+//   sendmotion_pub.publish(my_joint_position);
+// }
 
-
-void callback1(const ros::TimerEvent&)
-{
-  ROS_INFO("Callback 1 triggered");
-  // my_joint_position.position.a1 = 0;
-  // my_joint_position.position.a2 = -1.5;
-  // my_joint_position.position.a3 = 0;
-  // my_joint_position.position.a4 = 0;
-  // my_joint_position.position.a5 = 0;
-  // my_joint_position.position.a6 = 0;
-  // my_joint_position.position.a7 = 0;
-  sendmotion_pub.publish(my_joint_position);
-}
-
-void callback2(const ros::TimerEvent&)
-{
-  ROS_INFO("Callback 2 triggered");
-  my_joint_position.position.a1 = 0;
-  my_joint_position.position.a2 = 0;
-  my_joint_position.position.a3 = 0;
-  my_joint_position.position.a4 = 0;
-  my_joint_position.position.a5 = 0;
-  my_joint_position.position.a6 = 0;
-  sendmotion_pub.publish(my_joint_position);
-}
+// void callback2(const ros::TimerEvent&)
+// {
+//   ROS_INFO("Callback 2 triggered");
+//   my_joint_position.position.a1 = 0;
+//   my_joint_position.position.a2 = 0;
+//   my_joint_position.position.a3 = 0;
+//   my_joint_position.position.a4 = 0;
+//   my_joint_position.position.a5 = 0;
+//   my_joint_position.position.a6 = 0;
+//   sendmotion_pub.publish(my_joint_position);
+// }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "sendmotion");
-
-  // ros::Publisher sendmotion_pub = 
-  //   n.advertise<iiwa_msgs::JointPosition>("iiwa/command/JointPosition", 1000);
-
+  ros::NodeHandle n;
+  iiwa_msgs::JointPosition my_joint_position;
+  ros::Publisher sendmotion_pub = n.advertise<iiwa_msgs::JointPosition>("iiwa/command/JointPosition", 1000);
   ros::Rate loop_rate(10);
-
-  // iiwa_msgs::JointPosition my_joint_position;
 
   int count = 0;
   double speed = 0.1;
@@ -56,33 +48,32 @@ int main(int argc, char **argv)
   {
     ROS_INFO("count %d", count);
 
-    ros::Timer timer1 = n.createTimer(ros::Duration(0.1), callback1);
-    ros::Timer timer2 = n.createTimer(ros::Duration(1.0), callback2);
+    // ros::Timer timer1 = n.createTimer(ros::Duration(0.1), callback1);
+    // ros::Timer timer2 = n.createTimer(ros::Duration(1.0), callback2);
 
-    // my_joint_position.position.a1 = 0;
-    // my_joint_position.position.a2 = -1.5;
-    // my_joint_position.position.a3 = 0;
-    // my_joint_position.position.a4 = 0;
-    // my_joint_position.position.a5 = 0;
-    // my_joint_position.position.a6 = 0;
-    // my_joint_position.position.a7 = 0;
-    // sendmotion_pub.publish(my_joint_position);
-    // ros::Duration(10).sleep();
-    // ROS_INFO("NEXT\n");
-    // my_joint_position.position.a1 = 0;
-    // my_joint_position.position.a2 = 0;
-    // my_joint_position.position.a3 = 0;
-    // my_joint_position.position.a4 = 0;
-    // my_joint_position.position.a5 = 0;
-    // my_joint_position.position.a6 = 0;
-    // my_joint_position.position.a7 = 0;
-    // sendmotion_pub.publish(my_joint_position);
-    ros::Duration(10).sleep();
+    my_joint_position.position.a1 = 0;
+    my_joint_position.position.a2 = -1.5;
+    my_joint_position.position.a3 = 0;
+    my_joint_position.position.a4 = 0;
+    my_joint_position.position.a5 = 0;
+    my_joint_position.position.a6 = 0;
+    my_joint_position.position.a7 = 0;
+    sendmotion_pub.publish(my_joint_position);
+    ros::Duration(8).sleep();
+    ROS_INFO("NEXT\n");
+    my_joint_position.position.a1 = 0;
+    my_joint_position.position.a2 = 0;
+    my_joint_position.position.a3 = 0;
+    my_joint_position.position.a4 = 0;
+    my_joint_position.position.a5 = 0;
+    my_joint_position.position.a6 = 0;
+    my_joint_position.position.a7 = 0;
+    sendmotion_pub.publish(my_joint_position);
+    ros::Duration(8).sleep();
     ROS_INFO("FINISH\n");
     ros::spinOnce();
     loop_rate.sleep();
     ++count;
-    break;
   }
   ROS_INFO("BREAK\n");
 
